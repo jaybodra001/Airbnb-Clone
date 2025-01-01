9e919918f5b0bde2706aa7d50cca06f74f99497e
jay bodra <jaybodra01@gmail.com>
2025-01-01 06:17:39 +0530

    model, layout, button file created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
  
 export const metadata = {
   title: "Aribnb",
@@ -19,7 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="{font.className}">
-        <Navbar />
+        <ClientOnly>
+           <Modal isOpen title="iam jay" />
+           <Navbar />
+        </ClientOnly>
+        
         {children}
       </body>
     </html>
